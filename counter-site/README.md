fa834f0cabca3df4c19de44b6e863f7ca3304083
Arun Kumar <34161881+Arun-Kumar-D@users.noreply.github.com>
2023-01-19 18:35:27 +0530

    Initial Commit

---
+++ counter-site/README.md
@@ -0,0 +1,1 @@
+Counter appplication using JavaScript
---
+++ password-generator/index.js
@@ -0,0 +1,13 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+function generate(){
+let newPassword = document.querySelector(".new-password");
+newPassword.textContent = ""
+
+for(let i = 0; i < 10; i ++){
+let randomPassword = Math.floor(Math.random()*characters.length)
+newPassword.textContent += characters[randomPassword]
+}
+
+}
